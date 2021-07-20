f3d4ca2a2c8f820996c26c9a2abb627228155250
Harshada <harshada.punjabi@gmail.com>
2021-07-20 12:33:58 +0530

    deleted aapnav

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,6 @@ import 'package:flutter_base_architecture/ui/base_widget.dart';
 import 'package:graphql/client.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
-
-
-import 'package:ricky_n_morty/app_nav.dart';
-
 import 'di/providers.dart';
 import 'flutter_graphql_landing_page_application.dart';
 import 'flutter_graphql_landing_page_application_viewmodel.dart';
@@ -47,25 +43,4 @@ class MainAppWidget extends StatelessWidget {
         });
   }
 }
-class MyApp extends StatelessWidget {
-  final ValueNotifier<GraphQLClient> graphQLClient;
 
-  const MyApp({Key key, this.graphQLClient}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return GraphQLProvider(
-      client: graphQLClient,
-      child: MaterialApp(
-          title: 'Rick and Morty App',
-          theme: ThemeData.light().copyWith(
-              primaryColor: Colors.white,
-              accentColor: Colors.lightGreenAccent,
-              visualDensity: VisualDensity.adaptivePlatformDensity),
-          darkTheme: ThemeData.dark().copyWith(
-              accentColor: Colors.lightGreenAccent,
-              visualDensity: VisualDensity.adaptivePlatformDensity),
-          themeMode: ThemeMode.system,
-          home: Appnav()),
-    );
-  }
-}
